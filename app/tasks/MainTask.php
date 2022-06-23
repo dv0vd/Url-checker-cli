a2ad2bd33cc1f373df98fc7b317b69cfe76da91b
Viacheslav <dvo@tuta.io>
2022-06-23 23:03:09 +0300

    Added background url checking

--- app/tasks/MainTask.php
+++ app/tasks/MainTask.php
@@ -8,15 +8,44 @@ class MainTask extends \Phalcon\Cli\Task
         echo "Congratulations! You are now flying with Phalcon CLI!";
     }
 
+    public function repeatAction($url_id, $repeats) {
+        $url = Urls::findFirstByUrlId($url_id);
+        if($url == null) return;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        for($i = 2; $i < $repeats + 1; $i++) {
+            sleep(60);
+            $response = $this -> curl($ch, $url);
+            $check = new Checks();
+            $check -> datetime = date('Y-m-d H:i:s');
+            $check -> url_id = $url -> url_id;
+            $check -> try_no = $i;
+            if($response == 0) {
+                $check -> http = "err";
+            } else {
+                $check -> http = $response;
+            }
+            $check -> save();
+            if($response != 0) break;
+        }
+        curl_close($ch);
+        if(is_file("null"))
+            unlink("null");
+    }
+
+    private function curl($ch, $url){
+        curl_setopt($ch, CURLOPT_URL, $url -> url);
+        curl_exec($ch);
+        return curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    }
+
     public function checkAction($freq){
         $urls = Urls::findByFreq($freq);
         $unavailable = [];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         foreach($urls as $url) {
-            curl_setopt($ch, CURLOPT_URL, $url -> url);
-            curl_exec($ch);
-            $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $response = $this -> curl($ch, $url);
             $check = new Checks();
             $check -> datetime = date('Y-m-d H:i:s');
             $check -> url_id = $url -> url_id;
@@ -25,16 +54,15 @@ class MainTask extends \Phalcon\Cli\Task
                 $check -> http = $response;
             } else {
                 $check -> http = "err";
+                array_push($unavailable, $url);
             }
             $check -> save();
-        
         }
-        // $optArray = array(
-        //     CURLOPT_URL => $url,
-        //     CURLOPT_RETURNTRANSFER => true
-        // );
-        // curl_setopt_array($ch, $optArray);
-        // echo $response;
+        foreach($unavailable as $url){
+            $url_id = $url -> url_id;
+            $url_repeats = $url -> repeats;
+            exec("/usr/bin/php /var/www/cli/app/bootstrap.php main repeat $url_id $url_repeats > null &");
+        }
         curl_close($ch);
     }
 
